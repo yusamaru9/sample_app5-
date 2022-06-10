5c025e2ab026b54498b18264197aaf22b4f32a25
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-06-10 11:13:14 +0000

    [Update] 3-3復習

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,27 @@
 class ListsController < ApplicationController
   def new
+    @List = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
